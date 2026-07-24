51016a4f2b02e9348fe09603e1eae47038e5301c
Aatif Khan <148757000+aatifkhan786@users.noreply.github.com>
2026-07-25 00:34:26 +0530

    Time: 0 ms (100%), Space: 11.1 MB (72.64%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -12,28 +12,49 @@ class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
         
-        vector<int>ans;
+        // vector<int>ans;
+        // ListNode* temp=head;
+        // while(temp!=NULL){
+        //     ans.push_back(temp->val);
+        //     temp=temp->next;
+        // }
+        // int n=ans.size()-1;
+        // int i=left-1;
+        // int j=right-1;
+        // while(i<j){
+        //     swap(ans[i],ans[j]);
+        //     i++;
+        //     j--;
+        // }
+        // temp=head;
+        // int k=0;
+        // while(temp!=NULL){
+        //     temp->val=ans[k];
+        //     k++;
+        //     temp=temp->next;
+        // }
+        // return head;
+        stack<int>st;
         ListNode* temp=head;
+        int i=0;
         while(temp!=NULL){
-            ans.push_back(temp->val);
+            if(i>left-2 && i<right){
+                st.push(temp->val);
+            }
+            i++;
             temp=temp->next;
         }
-        int n=ans.size()-1;
-        int i=left-1;
-        int j=right-1;
-        while(i<j){
-            swap(ans[i],ans[j]);
-            i++;
-            j--;
-        }
         temp=head;
-        int k=0;
+        i=0;
         while(temp!=NULL){
-            temp->val=ans[k];
-            k++;
+            if(!st.empty() && i>left-2 && i<right){
+                temp->val=st.top();
+                cout<<st.top();
+                st.pop();
+            }
+            i++;
             temp=temp->next;
         }
         return head;
-
     }
 };
