38b709816e0200dd9dd0c09c36595904f758de52
Aatif Khan <148757000+aatifkhan786@users.noreply.github.com>
2026-07-28 09:49:50 +0530

    Time: 0 ms (100%), Space: 17.1 MB (33.31%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -16,65 +16,65 @@ public:
         if (head == NULL || head->next == NULL || k == 0)
             return head;
         
-        // vector<int> arr;
+        vector<int> arr;
 
-        // ListNode* temp = head;
+        ListNode* temp = head;
 
-        // // Linked List -> Array
-        // while (temp != NULL) {
-        //     arr.push_back(temp->val);
-        //     temp = temp->next;
-        // }
+        // Linked List -> Array
+        while (temp != NULL) {
+            arr.push_back(temp->val);
+            temp = temp->next;
+        }
 
-        // int n = arr.size();
-        // k = k % n;
+        int n = arr.size();
+        k = k % n;
 
-        // vector<int> ans(n);
+        vector<int> ans(n);
 
-        // // Right Rotate
-        // for (int i = 0; i < n; i++) {
-        //     ans[(i + k) % n] = arr[i];
-        // }
+        // Right Rotate
+        for (int i = 0; i < n; i++) {
+            ans[(i + k) % n] = arr[i];
+        }
 
-        // // Array -> Linked List
-        // temp = head;
-        // int i = 0;
+        // Array -> Linked List
+        temp = head;
+        int i = 0;
 
-        // while (temp != NULL) {
-        //     temp->val = ans[i];
-        //     i++;
-        //     temp = temp->next;
-        // }
-
-        // return head;
-
-
-
-
-
-        ListNode* temp=head;
-        int i=1;
-        while(temp->next!=NULL){
+        while (temp != NULL) {
+            temp->val = ans[i];
             i++;
-            temp=temp->next;
+            temp = temp->next;
+        }
+
+        return head;
+
+
+
+
+
+        // ListNode* temp=head;
+        // int i=1;
+        // while(temp->next!=NULL){
+        //     i++;
+        //     temp=temp->next;
             
-        }
-        temp->next=head;
-        int a=k%i;//kitne last se move karne hai
-        int b=i-a-1;//total elment-elemnts moved from last 
-        int c=b+1;
-        temp=head;
-        while(c>0){
-            temp=temp->next;
-            c--;
-        }
-        ListNode* curr=head;
-        while(b>0){
-            curr=curr->next;
-            b--;
-        }
-        curr->next=NULL;
-        return temp;
+        // }
+        // temp->next=head;
+        // int a=k%i;//kitne last se move karne hai
+        // int b=i-a-1;//total elment-elemnts moved from last 
+        // int c=b+1;
+        // temp=head;
+        // while(c>0){
+        //     temp=temp->next;
+        //     c--;
+        // }
+        // ListNode* curr=head;
+        // while(b>0){
+        //     curr=curr->next;
+        //     b--;
+        // }
+        // curr->next=NULL;
+        // return temp;
 
     }
 };
