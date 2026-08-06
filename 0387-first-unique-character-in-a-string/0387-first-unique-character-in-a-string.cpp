16d6e1814816010380e2956cc211bfa37ace3abb
Aatif Khan <148757000+aatifkhan786@users.noreply.github.com>
2026-08-06 19:33:09 +0530

    Time: 26 ms (9.43%), Space: 15 MB (21.57%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_map<char, int> freq;
-        for (int i = 0; i < s.size(); i++) {
-            freq[s[i]]++;
+        // unordered_map<char, int> freq;
+        // for (int i = 0; i < s.size(); i++) {
+        //     freq[s[i]]++;
+        // }
+        // for (int i = 0; i < s.size(); i++) {
+        //     if (freq[s[i]] == 1) {
+        //         return i;
+        //     }
+        // }
+        // return -1;
+   
+        unordered_set<char>seen;
+        unordered_set<char>duplicate;
+
+        for(int i=0;i<s.size();i++){
+            char ch=s[i];
+            if(seen.find(ch) == seen.end()){
+                seen.insert(ch);
+            }else{
+                duplicate.insert(ch);
+            }
         }
-        for (int i = 0; i < s.size(); i++) {
-            if (freq[s[i]] == 1) {
+        for(int i=0;i<s.size();i++){
+            char ch=s[i];
+            if(duplicate.find(ch)==seen.end()){
                 return i;
+                break;
             }
         }
         return -1;
-        // int i = 0;
-        // int j = s.size();
-        // while (i < j) {
-        //     if (s[i] != s[j]) {
-        //         j--;
-        //     } else {
-        //         i++;
-        //     }
-        // }
     }
 };
